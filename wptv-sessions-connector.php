eb4283d28d60ff4c4019438ad34ed5b8f39b1baa
Carlos Fischer <carlos@climbingflex.de>
2018-07-28 11:46:56 +0200

    draft first plugin file

---
+++ wptv-sessions-connector.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WordPress TV Sessions Connector
+Plugin URI:
+Description:  WordPress Plugin to connect Sessions and Video Hardware
+Version:      1.0.0
+Author:       WordPress TV German Team
+Author URI:
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wptv-sessions
+Domain Path:  /languages
+*/
